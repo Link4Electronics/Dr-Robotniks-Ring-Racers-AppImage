13bbd6728bca75b88b73a89a1008aab2bd017234
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:38:09 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q ringracers | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/org.kartkrew.RingRacers.png
 export DESKTOP=/usr/share/applications/org.kartkrew.RingRacers.desktop
